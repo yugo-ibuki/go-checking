6c795d2c7d43e49eda75b7ca71364708a512027e
yugo-ibuki <y.ibuki91@gmail.com>
2022-05-19 00:22:25 +0900

    Tagを追加

---
+++ Tag/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// タグとして定義する事ができる
+
+type Organization struct {
+	Name     string `label:"名前" json:"name" validate:"required"`
+	Language string `label:"言語" json:"language" validate:"required"`
+}
+
+// フォーム情報やJSONのリクエストを解析して構造体にマッピングし、問題なければバリデーションで入力値を検証し、最後にデータベースに書き込むということを1つの構造体で実現できる。
+
+func main() {
+	organization := Organization{
+		Name:     "gumi",
+		Language: "Elixir",
+	}
+	tag := reflect.TypeOf(organization).Field(0).Tag
+	fmt.Println(tag.Get("label"))      // -> 名前
+	fmt.Println(tag.Get("json"))       // -> name
+	fmt.Println(tag.Get("validate"))   // -> required
+	fmt.Println(organization.Name)     // -> gumi
+	fmt.Println(organization.Language) // -> Elixir
+}
